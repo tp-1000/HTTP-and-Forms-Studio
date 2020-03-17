96f81fc72013511ce61d0cf86e88d9738e8a9800
Thomas Petty <thomaspetty@Thomass-iMac.local>
2020-03-17 15:55:45 -0500

    Hammering out javascript validation

--- script.js
+++ script.js
@@ -1,19 +1,38 @@
 // TODO: create a handler
 window.addEventListener("load", function(){
-    let engine = {
+    const engineList = {
         goolge: "https://www.google.com/search",
         duck: "https://duckduckgo.com/",
         bing: "https://www.bing.com/search",
         ask: "https://www.ask.com/web"
     }
     
-    // get from element from DOM to set action
-    let form = this.document.querySelector("form")
-    let search = this.document.querySelector("input")
-    window.addEventListener("submit", function (event) {
-        //inputthis.addEventListener
-        alert(document.request.body);
+    // get elements from DOM
+    const form = this.document.querySelector("form")
+    const search_engines = document.querySelectorAll('input[name="search_engine"]');
+    const button = this.document.querySelector('button');
+    
+    // submit will check radios for true and set action attribute for the form.
+    button.addEventListener("click", function (event) {
+        try {
+            const search_input = docudsment.querySelector('input[id=q]');
+            alert(search_input.value);
+        } catch (error) {
+            alert(error);
+        }
+        
+        
+        if (search_input.value === "") {
+            event.defaultPrevented();
+            alert("field required");
+            break;
+        }
+        for (let engine of search_engines) {
+            if(engine.checked === true) {
+                let action_url = engineList[engine.value];
+                form.setAttribute("action", action_url)
+                // break;
+            }
+        }
     })
-    //when a radio is selected that value is the one i want
-    //so javascript needs access to that value
 });
