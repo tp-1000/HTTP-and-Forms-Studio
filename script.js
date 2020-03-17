d058d605ed1495b24530daff8026eeb73c1333c4
Thomas Petty <thomaskpetty@gmail.com>
2020-03-17 04:04:02 -0500

    trying to get input from radios

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
 // TODO: create a handler
 window.addEventListener("load", function(){
-    //let engine =  
+    let engine = {
+        goolge: "https://www.google.com/search",
+        duck: "https://duckduckgo.com/",
+        bing: "https://www.bing.com/search",
+        ask: "https://www.ask.com/web"
+    }
+    
+    // get from element from DOM to set action
+    let form = this.document.querySelector("form")
+    let search = this.document.querySelector("input")
+    window.addEventListener("submit", function (event) {
+        inputthis.addEventListener
+        alert(event.target.getAttribute("name"));
+    })
+    //when a radio is selected that value is the one i want
+    //so javascript needs access to that value
 });
-
-alert("here");
