7989ae3a744b53cc29c4eb1a8622a85819d60206
T-Petty <thomaskpetty@gmail.com>
2020-03-17 19:08:07 -0500

    Working version of searchengine selector

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // TODO: create a handler
 window.addEventListener("load", function(){
     const engineList = {
-        goolge: "https://www.google.com/search",
+        google: "https://www.google.com/search",
         duck: "https://duckduckgo.com/",
         bing: "https://www.bing.com/search",
         ask: "https://www.ask.com/web"
@@ -9,30 +9,29 @@ window.addEventListener("load", function(){
     
     // get elements from DOM
     const form = this.document.querySelector("form")
-    const search_engines = document.querySelectorAll('input[name="search_engine"]');
     const button = this.document.querySelector('button');
     
     // submit will check radios for true and set action attribute for the form.
     button.addEventListener("click", function (event) {
-        try {
-            const search_input = docudsment.querySelector('input[id=q]');
-            alert(search_input.value);
-        } catch (error) {
-            alert(error);
-        }
-        
-        
+        const search_input = document.querySelector('input[id=q]');
+        const search_engine = document.querySelector('input[name=search_engine]:checked').value;
+
+        // search_engine
         if (search_input.value === "") {
-            event.defaultPrevented();
-            alert("field required");
-            break;
+            event.preventDefault();
+            alert("field required!");
+
+        } else {
+            let action_url = engineList[search_engine];
+            form.setAttribute("action", action_url)
+        
         }
-        for (let engine of search_engines) {
-            if(engine.checked === true) {
-                let action_url = engineList[engine.value];
-                form.setAttribute("action", action_url)
-                // break;
-            }
-        }
+            // for (let engine of search_engines) {
+            //     if(engine.checked === true) {
+            //         let action_url = engineList[engine.value];
+            //         form.setAttribute("action", action_url)
+            //     }
+            // }
+        
     })
 });
